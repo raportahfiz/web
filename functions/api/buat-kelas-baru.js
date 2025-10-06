35c7807097140100e29a5eb70e292de4b5d5d15d
raportahfiz <raportahfiz@gmail.com>
2025-10-06 13:28:06 +0700

    Batch replace: "muqoddammtb" -> "raportahfiz" | buat-kelas-baru.js | hits=1

--- functions/api/buat-kelas-baru.js
+++ functions/api/buat-kelas-baru.js
@@ -9,7 +9,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
-const OWNER_REPO = "muqoddammtb/server";
+const OWNER_REPO = "raportahfiz/server";
 const BRANCH = "main";
 
 const ghHeaders = (token) => ({
