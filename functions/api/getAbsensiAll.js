60b72e165f0057d85cb6e1e2175b1ad44f6e3de1
raportahfiz <raportahfiz@gmail.com>
2025-10-06 13:28:16 +0700

    Batch replace: "muqoddammtb" -> "raportahfiz" | getAbsensiAll.js | hits=1

--- functions/api/getAbsensiAll.js
+++ functions/api/getAbsensiAll.js
@@ -8,7 +8,7 @@ const CORS = {
   "Access-Control-Allow-Headers": "Content-Type,Authorization",
 };
 
-const OWNER_REPO = "muqoddammtb/server";
+const OWNER_REPO = "raportahfiz/server";
 const DEFAULT_BRANCH = "main";
 
 const ghHeaders = (token) => ({
